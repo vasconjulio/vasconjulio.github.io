da19022de2a4931448abd4ef9116cf97a87acea4
Julio Cesar Santos de Vasconcelos <julioc43@gmail.com>
2022-02-21 20:49:09 -0300

    21/02/2022

--- js-midia/scripts.js
+++ js-midia/scripts.js
@@ -8,3 +8,13 @@ function toggleMenu() {
 
 
 btnMobile.addEventListener('click', toggleMenu);
+
+
+
+// Topo Scroll
+
+window.addEventListener("scroll", function(){
+	const header = document.querySelector("#header");
+	header.classList.toggle("topo-scroll", window.scrollY > 2);
+})
+
